a08e02b07f58598ac0789863d0592c080c970ada
Kilian <26754107+ImNotKilian@users.noreply.github.com>
2022-11-06 21:46:13 -0500

    Update getLocationCoordinates.js

--- pages/api/map/getLocationCoordinates.js
+++ pages/api/map/getLocationCoordinates.js
@@ -1,5 +1,6 @@
 const getLocationCoordinates = async (req, res) => {
   const mapboxUrl = `${process.env.MAPBOX_PLACES_API_URL}/${req.body.location}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`
+  console.log(process.env)
 
   try {
     const response = await fetch(mapboxUrl)
