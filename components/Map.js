9250936e0e47ce614962210c7211780cfbae35e0
Kilian <26754107+ImNotKilian@users.noreply.github.com>
2022-11-06 21:21:53 -0500

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -6,7 +6,7 @@ const style = {
   wrapper: `flex-1 h-full w-full`,
 }
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = ('pk.eyJ1IjoiYjAwbXgwciIsImEiOiJjbGE2MjAzc2MwazFoM29sbGx0azFyNHVlIn0.jE1nMuAm0SBiJvkLx4at4Q');
 
 const Map = () => {
   const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
